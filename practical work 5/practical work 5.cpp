690371374fc050081c5897c4f5e2f57ec7fd3b85
Sergey <30868885+zloysergunya@users.noreply.github.com>
2019-09-12 20:03:54 +0700

    Update practical work 5.cpp

--- practical work 5/practical work 5.cpp
+++ practical work 5/practical work 5.cpp
@@ -1,75 +1,76 @@
 #include <iostream>
 #include <ctime>
 #include <vector>
-
+ 
 using namespace std;
-
+ 
 vector<int> gnomeSort(vector<int> sequence) {
-	for (int i = 0; i + 1 < sequence.size(); ++i) {
-		if (sequence[i] > sequence[i+1]) {
-			int temp = sequence[i]; 
-			sequence[i] = sequence[i + 1]; 
-			sequence[i + 1] = temp;
-
-			if (i != 0) i -= 2;
-		}
-	}
-	return sequence;
+    for (int i = 0; i + 1 < sequence.size(); ++i) {
+        if (sequence[i] > sequence[i + 1]) {
+            int temp = sequence[i];
+            sequence[i] = sequence[i + 1];
+            sequence[i + 1] = temp;
+ 
+            if (i != 0) i -= 2;
+        }
+    }
+    return sequence;
 }
-
+ 
 vector<int> quickSort(vector<int> sequence, int low, int high) {
-	int i = low;
-	int j = high;
-	int x = sequence[(low + high) / 2];
-
-	do {
-		while (sequence[i] < x) ++i;
-		while (sequence[j] > x) --j;
-
-		if (i <= j) {
-			int temp = sequence[i];
-			sequence[i] = sequence[i + 1];
-			sequence[i + 1] = temp;
-			i++; 
-			j--;
-		}
-	} while (i < j);
-
-	if (low < j) quickSort(sequence, low, j);
-	if (i < high) quickSort(sequence, i, high);
-
-	return sequence;
+    int i = low;
+    int j = high;
+    int x = sequence[(low + high) / 2];
+       
+    do {
+        while (sequence[i] < x) ++i;
+        while (sequence[j] > x) --j;
+ 
+        if (i <= j) {
+            int temp = sequence[i];
+            sequence[i] = sequence[j];
+            sequence[j] = temp;
+            i++; j--;
+        }
+    } while (i < j);
+ 
+    if (low < j) quickSort(sequence, low, j);  
+    if (i < high) quickSort(sequence, i, high);
+ 
+    return sequence;
 }
-
+ 
 int main() {
-	setlocale(LC_ALL, "");
-	srand(time(0));
-
-	vector<int> sequence;
-	int n;
-	cout << "Введите количество элементов: ";
-	cin >> n;
-
-	for (int i = 0; i < n; i++) sequence.push_back(rand());
-
-	for (int& i : sequence) cout << i << " ";
-
-	time_t startTime, finishTime;
-
-	cout << endl << endl;
-
-	startTime = time(NULL);
-	vector<int> newSequence = gnomeSort(sequence);
-	finishTime = time(NULL);
-	for (int& i : newSequence) cout << i << " ";
-	cout << endl << "Время гномьей сортировки: " << difftime(finishTime, startTime) << endl;
-
-	startTime = time(NULL);
-	vector<int> newSecSequence = quickSort(sequence, 0, sequence.size());
-	finishTime = time(NULL);
-	for (int& i : newSecSequence) cout << i << " ";
-	cout << endl << "Время быстрой сортировки: " << difftime(finishTime, startTime) << endl;
-
-
-	return 0;
+    setlocale(LC_ALL, "");
+    srand(time(0));
+ 
+    vector<int> sequence;
+    int n;
+    cout << "Введите количество элементов: ";
+    cin >> n;
+ 
+    for (int i = 0; i < n; i++) sequence.push_back(rand());
+ 
+    for (int& i : sequence) cout << i << " ";
+ 
+    time_t startTime, finishTime;
+ 
+    cout << endl << endl;
+ 
+    startTime = time(NULL);
+    vector<int> newSequence = gnomeSort(sequence);
+    finishTime = time(NULL);
+    cout << "Результат гномьей сортировки: " << endl;
+    for (int& i : newSequence) cout << i << " ";
+    cout << endl << "Время гномьей сортировки: " << difftime(finishTime, startTime)/1000. << endl;
+ 
+    startTime = time(NULL);
+    vector<int> newSecSequence = quickSort(sequence, 0, sequence.size() - 1);
+    finishTime = time(NULL);
+    cout << "Результат быстрой сортировки: " << endl;
+    for (int& i : newSecSequence) cout << i << " ";
+    cout << endl << "Время быстрой сортировки: " << difftime(finishTime, startTime)/1000. << endl;
+ 
+ 
+    return 0;
 }
