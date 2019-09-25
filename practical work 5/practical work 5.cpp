49ac71b4467adfc85ef6229036ee323ce4e40d48
Sergey <30868885+zloysergunya@users.noreply.github.com>
2019-09-25 18:59:37 +0700

    Update practical work 5.cpp

--- practical work 5/practical work 5.cpp
+++ practical work 5/practical work 5.cpp
@@ -17,11 +17,11 @@ vector<int> gnomeSort(vector<int> sequence) {
 	return sequence;
 }
 
-void quickSort(vector<int> &sequence, int low, int high) {
+void quickSort(vector<int> & sequence, int low, int high) {
 	int i = low;
 	int j = high;
 	int x = sequence[(low + high) / 2];
-		
+
 	do {
 		while (sequence[i] < x) ++i;
 		while (sequence[j] > x) --j;
@@ -34,13 +34,51 @@ void quickSort(vector<int> &sequence, int low, int high) {
 		}
 	} while (i < j);
 
-	if (low < j) quickSort(sequence, low, j);	
+	if (low < j) quickSort(sequence, low, j);
 	if (i < high) quickSort(sequence, i, high);
 }
 
-// Сортировка кучей
-void heapSort(vector<int>& sequence) {
+// Функция "просеивания" через кучу - формирование кучи
+void siftDown(vector<int>& sequence, int root, int bottom) {
+	int maxChild; // индекс максимального потомка
+	int done = 0; // флаг того, что куча сформирована
 
+	// Пока не дошли до последнего ряда
+	while (root * 2 <= bottom && !done) {
+		if (root * 2 == bottom) { // если мы в последнем ряду,
+			maxChild = root * 2; // запоминаем левый потомок
+		}
+		// иначе запоминаем больший потомок из двух
+		else if (sequence[root * 2] > sequence[root * 2 + 1]) {
+			maxChild = root * 2;
+		}
+		else {
+			maxChild = root * 2 + 1;
+		}
+		// если элемент вершины меньше максимального потомка
+		if (sequence[root] < sequence[maxChild]) {
+			int temp = sequence[root]; // меняем их местами
+			sequence[root] = sequence[maxChild];
+			sequence[maxChild] = temp;
+			root = maxChild;
+		}
+		else // иначе
+			done = 1; // пирамида сформирована
+	}
+}
+
+// Сортировка кучей
+void heapSort(vector<int> & sequence) {
+	for (int i = (sequence.size() / 2) - 1; i >= 0; i--) {
+		siftDown(sequence, i, sequence.size() - 1);
+	}
+	// Просеиваем через пирамиду остальные элементы
+	for (int i = sequence.size() - 1; i >= 1; i--) {
+		int temp = sequence[0];
+		sequence[0] = sequence[i];
+		sequence[i] = temp;
+		siftDown(sequence, 0, i - 1);
+	}
 }
 
 int main() {
@@ -66,7 +104,6 @@ int main() {
 	cout << "Результат гномьей сортировки: " << endl;
 	for (int& i : newSequence) cout << i << " ";
 	cout << endl << "Время гномьей сортировки: " << difftime(finishTime, startTime)/1000. << endl;
-
 	startTime = time(NULL);
 	quickSort(sequence, 0, sequence.size() - 1);
 	finishTime = time(NULL);
@@ -77,9 +114,9 @@ int main() {
 	startTime = time(NULL);
 	heapSort(sequence);
 	finishTime = time(NULL);
-	cout << "Результат быстрой сортировки: " << endl;
+	cout << "Результат сортировки кучей: " << endl;
 	for (int& i : sequence) cout << i << " ";
-	cout << endl << "Время быстрой сортировки: " << difftime(finishTime, startTime) / 1000. << endl;
+	cout << endl << "Время сортировки кучей: " << difftime(finishTime, startTime) / 1000. << endl;
 
 	return 0;
 }
