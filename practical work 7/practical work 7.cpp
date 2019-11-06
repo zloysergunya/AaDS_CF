267486821c2036b3eadc095f3273d0c91467bde4
Sergey <30868885+zloysergunya@users.noreply.github.com>
2019-11-06 22:55:43 +0700

    Рефактор под потоки

--- practical work 7/practical work 7.cpp
+++ practical work 7/practical work 7.cpp
@@ -7,7 +7,7 @@
 #pragma warning(disable : 4996)
 
 using namespace std;
-void Simple_Merging_Sort(const char* name) {
+void simpleMergingSort(const char* name) {
 	int a1, a2, k, i, j, kol, tmp;
 	FILE *f, *f1, *f2;
 	kol = 0;
@@ -88,12 +88,95 @@ void Simple_Merging_Sort(const char* name) {
 	remove("smsort_2");
 }
 
+void simpleMergingSort2(const char* name) {
+	int a1, a2, k, i, j, kol, tmp;
+	fstream f, f1, f2;
+	FILE* f, * f1, * f2;
+	kol = 0;
+	if (!f.is_open()) {
+		printf("Исходный файл не может быть прочитан...");
+	}
+	else {
+		while (!f.eof()) {
+			f >> a1;
+			kol++;
+		}
+		f.close();
+	}
+	k = 1;
+	while (k < kol) {
+		f.open(name);
+		f1.open("smsort_1.txt");
+		f2.open("smsort_2.txt");
+		if (!f.eof()) f >> a1;
+
+		while (!f.eof()) {
+			for (i = 0; i < k && !f.eof(); i++) {
+				f1 << a1;
+				f >> a1;
+			}
+			for (j = 0; j < k && !f.eof(); j++) {
+				f2 << a1;
+				f >> a1;
+			}
+		}
+		f2.close();
+		f1.close();
+		f.close();
+
+		f.open(name);
+		f1.open("smsort_1.txt");
+		f2.open("smsort_2.txt");
+		if (!f1.eof()) f1 >> a1;
+		if (!f2.eof()) f2 >> a2;
+		while (!f1.eof() && !f2.eof()) {
+			i = 0;
+			j = 0;
+			while (i < k && j < k && !f1.eof() && !f2.eof()) {
+				if (a1 < a2) {
+					f << a1;
+					f1 >> a1;
+					i++;
+				}
+				else {
+					f << a2;
+					f2 >> a2;
+					j++;
+				}
+			}
+			while (i < k && !f1.eof()) {
+				f << a1;
+				f1 >> a1;
+				i++;
+			}
+			while (j < k && !f2.eof()) {
+				f << a2;
+				f2 >> a2;
+				j++;
+			}
+		}
+		while (!f1.eof()) {
+			f << a1;
+			f1 >> a1;
+		}
+		while (!f2.eof()) {
+			f << a2;
+			f >> a2;
+		}
+		f2.close();
+		f1.close();
+		f.close();
+		k *= 2;
+	}
+}
+
 
 int main() {
 	setlocale(LC_ALL, "");
-	/*const char* n = new char[6];
-	n = "in.txt";*/
-	Simple_Merging_Sort("in.txt");
+
+	simpleMergingSort2("input.txt");
+
+	cout << "Сортировка прошла успешно! Проверьте исходных файл!" << endl;
 
 	return 0;
 }
